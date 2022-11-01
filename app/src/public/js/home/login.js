01b588e0ba6abe3c8b2e1621f6a110b6a7d73ecb
JwaSangHeon <whk4051@naver.com>
2022-11-01 17:40:24 +0900

    dom 학습 및 defer,async js 차이점 이해

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,6 +1,18 @@
 "use strict";
+
+// 변수
+const id = document.querySelector("#id");
+const password = document.querySelector("#password");
 const loginBtn = document.querySelector("#loginBtn");
-loginBtn.addEventListener("click", () => {
-  console.log("click");
-});
-console.log("login!!!!!!");
+
+// 함수
+const onClickLoginBtn = () => {
+  const req = {
+    id: id.value,
+    password: password.value,
+  };
+  console.log(req);
+};
+
+// 이벤트
+loginBtn.addEventListener("click", onClickLoginBtn);
