f95528025b7a65dde48a80b9ed79ebcce33c7aad
JwaSangHeon <whk4051@naver.com>
2022-11-01 16:58:13 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -5,10 +5,10 @@ const express = require("express");
 const app = express();
 
 // Routing
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 // App settings
-app.set("views", "./views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 app.use("/", home); // use => 미들 웨어를 등록해주는 메서드.
